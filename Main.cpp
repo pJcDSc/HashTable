5fa87b55bc3ef4565eab3be681332976f355c21b
U-DESKTOP-QBU2FKV\Huanhuan <411036@bsd48.org>
2020-05-27 12:55:19 -0700

    Begin

--- Main.cpp
+++ Main.cpp
@@ -10,5 +10,16 @@ Hash table student list
 using namespace std;
 
 int main() {
+  cout << "Welcome to student list with hashtable" << endl;
+  cout << "Type help for more commands" << endl;
+  bool run = true;
+  char* input = new char();
+  while (run) {
+    cin.get(input, 30);
+    cin.clear();
+    cin.ignore(999, '\n');
+
+    run = parse(input);
+  }
   return 0;
 }
